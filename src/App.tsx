3a5db10552bad6bbc8c3ba5db5fc210832145c81
Hana Tokárová <htokarova@gmail.com>
2024-02-26 11:51:44 +0100

    Playing with the setup 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { ChakraProvider, theme } from "@chakra-ui/react"
+import { Button, ChakraProvider, theme } from "@chakra-ui/react"
 import { Canvas } from "@react-three/fiber"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <div style={{ width: "100vw", height: "100vh" }}>
+    <div style={{ width: "100vw", height: "100vh", position: "relative" }}>
       <Canvas>
+        <color attach="background" args={["white"]} />
         <pointLight intensity={0.01} />
-        <directionalLight color="purple" position={[0, 0, 5]} />
-        <mesh position={[0, 0, 0]} scale={1} rotation={[180, 0, 180]}>
-          <torusGeometry args={[2, 0.4, 8, 20, Math.PI * 2]} />
-
-          <meshPhongMaterial />
+        <directionalLight color="white" position={[0, 0, 5]} />
+        <mesh position={[0, 0, 0]} rotation={[180, 180, 180]}>
+          <torusGeometry radius={4} radialSegments={8} tubularSegments={10} />
+          <meshStandardMaterial flatShading={true} color="pink" />
         </mesh>
       </Canvas>
+      <div style={{ position: "absolute", top: "10px", left: "10px" }}>
+        <Button colorScheme="blue">Click Me</Button>
+      </div>
     </div>
   </ChakraProvider>
 )
