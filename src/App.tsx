47b7cc376ca0d17c6ad7f6b7fbc26a13d06af354
Hana Tokárová <htokarova@gmail.com>
2024-02-28 16:29:39 +0100

    OrbitControls work wohoo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import * as THREE from 'three';
 import { STLExporter } from 'three/examples/jsm/exporters/STLExporter';
 
+import { OrbitControls } from "@react-three/drei";
 import { FaSquare } from "react-icons/fa";
 import { HiOutlineDownload } from "react-icons/hi";
 import { Object3D } from "three";
@@ -15,7 +16,7 @@ const makeLissajousCurve3D = (nbSteps: number, s: number, a: number, b: number, 
 
   for (let t = -range / 2; t <= range / 2; t += stepSize) {
     const x = s * Math.sin(a * t);
-    const y = s * Math.sin(b * t + delta);
+    const y = (s / 2) * Math.sin(b * t + delta);
     const z = s * Math.sin(c * t + gamma);
 
     points.push(new THREE.Vector3(x, y, z));
@@ -27,7 +28,6 @@ const makeLissajousCurve3D = (nbSteps: number, s: number, a: number, b: number, 
 type LissajouCurveProps = {
   mesh: MeshRef;
   meshColor: string;
-  meshRotation: THREE.Euler;
 };
 
 const LissajouCurve = (props: LissajouCurveProps) => {
@@ -35,9 +35,9 @@ const LissajouCurve = (props: LissajouCurveProps) => {
   const path = new THREE.CatmullRomCurve3(points, true, "centripetal");
 
   return (
-    <mesh ref={props.mesh} position={[0, 0, 0]} rotation={props.meshRotation}>
-      <tubeGeometry attach="geometry" args={[path, 256, 0.05, 20, true]} />
-      <meshStandardMaterial attach="material" flatShading={true} color={props.meshColor} />
+    <mesh ref={props.mesh} position={[0, 0, 0]} >
+      <tubeGeometry attach="geometry" args={[path, 512, 0.05, 20, true]} />
+      <meshStandardMaterial attach="material" flatShading={false} color={props.meshColor} />
     </mesh>
   );
 };
@@ -47,7 +47,6 @@ type MeshRef = React.RefObject<THREE.Mesh<THREE.BufferGeometry<THREE.NormalBuffe
 export const App = () => {
   const myMesh = React.useRef<THREE.Mesh>(null);
 
-  const meshRotation = new THREE.Euler(-180, 180, 0);
   const [meshColor, setMeshColor] = React.useState("white");
   const colors = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white"];
 
@@ -80,8 +79,10 @@ export const App = () => {
       <div style={{ width: "100vw", height: "100vh", position: "relative" }}>
         <Canvas >
           <color attach="background" args={["white"]} />
-          <directionalLight position={[0, 0, 5]} intensity={2} />
-          <LissajouCurve mesh={myMesh} meshColor={meshColor} meshRotation={meshRotation} />
+          <pointLight position={[0, 0, 0]} intensity={3} />
+          <ambientLight intensity={0.1} />
+          <LissajouCurve mesh={myMesh} meshColor={meshColor} />
+          <OrbitControls />
         </Canvas>
         <div style={{ position: "absolute", top: "10px", left: "10px" }}>
           <HStack marginBottom={2}>
@@ -97,7 +98,7 @@ export const App = () => {
 
           <Button
             rightIcon={<HiOutlineDownload />}
-            onClick={() => exportMesh(myMesh.current!.parent!)} // is this ok? i hope so
+            onClick={() => exportMesh(myMesh.current!.parent!)}
           >
             Export to .STL
           </Button>
