26eff9bbcc40123f40f9eace7845e33696c41a92
Hana Tokárová <htokarova@gmail.com>
2024-04-08 23:28:16 +0200

    Added obj and gltf exporter

--- src/components/Configurator.tsx
+++ src/components/Configurator.tsx
@@ -1,7 +1,9 @@
-import { Button, ChakraProvider, HStack, IconButton, Slider, SliderFilledTrack, SliderMark, SliderThumb, SliderTrack, Switch, theme } from "@chakra-ui/react";
+import { Button, HStack, IconButton, Select, Slider, SliderFilledTrack, SliderMark, SliderThumb, SliderTrack, Switch } from "@chakra-ui/react";
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import React, { useRef } from 'react';
 import * as THREE from 'three';
+import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter';
+import { OBJExporter } from 'three/examples/jsm/exporters/OBJExporter';
 import { STLExporter } from 'three/examples/jsm/exporters/STLExporter';
 
 import { OrbitControls } from "@react-three/drei";
@@ -80,11 +82,16 @@ export const Configurator = (props: ConfiguratorProps) => {
     link.click();
   }
 
-  const saveArrayBuffer = (buffer: DataView, filename: string) => {
+  const saveString = (text: string, filename: string) => {
+    save(new Blob([text], { type: 'text/plain' }), filename);
+  }
+
+
+  const saveArrayBuffer = (buffer: ArrayBuffer | DataView, filename: string) => {
     save(new Blob([buffer], { type: 'application/octet-stream' }), filename);
   }
 
-  const exportMesh = (mesh: Object3D) => {
+  const exportMeshSTL = (mesh: Object3D) => {
     const clonedMesh = mesh.clone();
 
     const rotationMatrix = new THREE.Matrix4().makeRotationX(Math.PI / 2);
@@ -92,9 +99,56 @@ export const Configurator = (props: ConfiguratorProps) => {
     clonedMesh.updateMatrixWorld();
 
     const exporter = new STLExporter();
-    const stlString = exporter.parse(clonedMesh, { binary: true });
+    const data = exporter.parse(clonedMesh, { binary: true });
 
-    saveArrayBuffer(stlString, clonedMesh.uuid + '.stl');
+    saveArrayBuffer(data, clonedMesh.uuid + '.stl');
+  };
+
+  const exportMeshOBJ = (mesh: Object3D) => {
+    const clonedMesh = mesh.clone();
+    const scene = mesh.parent!;
+
+    const exporter = new OBJExporter();
+    const data = exporter.parse(scene);
+
+    saveString(data, clonedMesh.uuid + '.obj');
+  };
+
+  const exportMeshGlTF = (mesh: Object3D) => {
+    const clonedMesh = mesh.clone();
+    const scene = mesh.parent!;
+
+    const options = {
+      trs: false,
+      onlyVisible: true,
+      binary: false,
+      maxTextureSize: 4096
+    };
+
+    const exporter = new GLTFExporter();
+    exporter.parse(
+      scene,
+      function (data) {
+        if (data instanceof ArrayBuffer) {
+
+          saveArrayBuffer(data, clonedMesh.uuid + '.glb');
+
+        } else {
+
+          const output = JSON.stringify(data, null, 2);
+          console.log(output);
+          saveString(output, clonedMesh.uuid + '.gltf');
+
+        }
+
+      },
+      function (error) {
+
+        console.log('An error happened during parsing', error);
+
+      },
+      options
+    );
   };
 
   const FollowCameraLight = () => {
@@ -121,88 +175,92 @@ export const Configurator = (props: ConfiguratorProps) => {
   }
 
   return (
-    <ChakraProvider theme={theme}>
-      <div style={{ width: "100vw", height: "80vh" }}>
-        <Canvas camera={{ fov: 50, near: 0.1, far: 1000, position: [75, 75, 0] }} >
+    <div style={{ width: "100vw", height: "80vh" }}>
+      <Canvas camera={{ fov: 50, near: 0.1, far: 1000, position: [75, 75, 0] }} >
 
-          <FollowCameraLight />
+        <FollowCameraLight />
 
-          <ambientLight
-            intensity={1}
-            color="dimgray"
-          />
+        <ambientLight
+          intensity={1}
+          color="dimgray"
+        />
 
-          {jewelryMesh!.render(numericParameters, booleanParameters, meshColor, myMesh)}
+        {jewelryMesh!.render(numericParameters, booleanParameters, meshColor, myMesh)}
 
-          <OrbitControls
-            enablePan={false}
-            enableRotate={true}
-            enableZoom={false}
-            enableDamping={true}
-            minPolarAngle={Math.PI / 2 - Math.PI / 5}
-            maxPolarAngle={Math.PI / 2 + Math.PI / 5}
-          />
-        </Canvas>
-        <div style={{ position: "absolute", top: "10px", left: "10px" }}>
-          <HStack marginBottom={2}>
-            {colors.map((buttonColor) =>
-              <IconButton
-                key={buttonColor}
-                aria-label='Change color'
-                icon={<FaSquare />}
-                color={buttonColor}
-                onClick={() => changeColor(buttonColor)}
-              />
-            )}
-          </HStack>
+        <OrbitControls
+          enablePan={false}
+          enableRotate={true}
+          enableZoom={false}
+          enableDamping={true}
+          minPolarAngle={Math.PI / 2 - Math.PI / 5}
+          maxPolarAngle={Math.PI / 2 + Math.PI / 5}
+        />
+      </Canvas>
+      <div style={{ position: "absolute", top: "10px", left: "10px" }}>
+        <HStack marginBottom={2}>
+          {colors.map((buttonColor) =>
+            <IconButton
+              key={buttonColor}
+              aria-label='Change color'
+              icon={<FaSquare />}
+              color={buttonColor}
+              onClick={() => changeColor(buttonColor)}
+            />
+          )}
+        </HStack>
 
-          <Button
-            key={"export"}
-            rightIcon={<HiOutlineDownload />}
-            onClick={() => exportMesh(myMesh.current!)}
-          >
-            Export to .STL
-          </Button>
+        <Button
+          key={"STLexport"}
+          rightIcon={<HiOutlineDownload />}
+          onClick={() => exportMeshSTL(myMesh.current!)}
+        >
+          Export to .STL
+        </Button>
 
-          {jewelryMesh && Object.entries(jewelryMesh.numericParameters).map(([parameterName, parameterDetails]) => (
-            <div key={parameterName}>
-              {parameterDetails.name}
-              <Slider
-                margin={2}
-                colorScheme='cyan'
-                value={numericParameters[parameterName]}
-                min={parameterDetails.min}
-                max={parameterDetails.max}
-                step={parameterDetails.step}
-                onChange={(newValue) => changeNumericParameter(parameterName, newValue)}>
+        <Select placeholder='Export to:' size='sm' variant='filled'>
+          <option value='stl' onClick={() => exportMeshSTL(myMesh.current!)}>.STL</option>
+          <option value='obj' onClick={() => exportMeshOBJ(myMesh.current!)}>.OBJ</option>
+          <option value='gltf' onClick={() => exportMeshGlTF(myMesh.current!)}>.glTF</option>
+        </Select>
 
-                <SliderMark value={parameterDetails.min} mt='1' fontSize='sm'>
-                  {parameterDetails.min}
-                </SliderMark>
-                <SliderMark value={parameterDetails.max} mt='1' fontSize='sm'>
-                  {parameterDetails.max}
-                </SliderMark>
-                <SliderTrack>
-                  <SliderFilledTrack />
-                </SliderTrack>
-                <SliderThumb />
-              </Slider>
-            </div>
-          ))}
+        {jewelryMesh && Object.entries(jewelryMesh.numericParameters).map(([parameterName, parameterDetails]) => (
+          <div key={parameterName}>
+            {parameterDetails.name}
+            <Slider
+              margin={2}
+              colorScheme='cyan'
+              value={numericParameters[parameterName]}
+              min={parameterDetails.min}
+              max={parameterDetails.max}
+              step={parameterDetails.step}
+              onChange={(newValue) => changeNumericParameter(parameterName, newValue)}>
 
-          {jewelryMesh && Object.entries(jewelryMesh.booleanParameters).map(([parameterName, parameterDetails]) => (
-            <div key={parameterName} >
-              {parameterDetails.name}
-              <Switch
-                margin={2}
-                colorScheme='cyan'
-                isChecked={booleanParameters[parameterName]}
-                onChange={(newValue) => changeBooleanParameter(parameterName, newValue.target.checked)}
-              />
-            </div>
-          ))}
-        </div>
+              <SliderMark value={parameterDetails.min} mt='1' fontSize='sm'>
+                {parameterDetails.min}
+              </SliderMark>
+              <SliderMark value={parameterDetails.max} mt='1' fontSize='sm'>
+                {parameterDetails.max}
+              </SliderMark>
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <SliderThumb />
+            </Slider>
+          </div>
+        ))}
+
+        {jewelryMesh && Object.entries(jewelryMesh.booleanParameters).map(([parameterName, parameterDetails]) => (
+          <div key={parameterName} >
+            {parameterDetails.name}
+            <Switch
+              margin={2}
+              colorScheme='cyan'
+              isChecked={booleanParameters[parameterName]}
+              onChange={(newValue) => changeBooleanParameter(parameterName, newValue.target.checked)}
+            />
+          </div>
+        ))}
       </div>
-    </ChakraProvider>
+    </div>
   );
 };
