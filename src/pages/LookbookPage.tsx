b47974c391f385eb52e052528e407e39ac0a9f4e
Hana Tokárová <htokarova@gmail.com>
2024-04-20 20:07:26 +0200

    Showcase has existing projects

--- src/pages/LookbookPage.tsx
+++ src/pages/LookbookPage.tsx
@@ -1,71 +1,6 @@
 import { Box, Image, Text, VStack } from '@chakra-ui/react';
-import { CollectionType, JewelryType } from '../components/collections/Collections';
-
-type Project = {
-    id: number;
-    image: string;
-    collection?: CollectionType;
-    jewelry?: JewelryType;
-};
-
-const projects: Project[] = [
-    {
-        id: 1,
-        image: 'https://source.unsplash.com/random/100x100',
-    },
-    {
-        id: 2,
-        image: 'https://source.unsplash.com/random/100x50',
-    },
-    {
-        id: 3,
-        image: 'https://source.unsplash.com/random/100x35',
-    },
-    {
-        id: 4,
-        image: 'https://source.unsplash.com/random/100x70',
-    },
-    {
-        id: 5,
-        image: 'https://source.unsplash.com/random/100x90',
-    },
-    {
-        id: 6,
-        image: 'https://source.unsplash.com/random/100x120',
-    },
-    {
-        id: 7,
-        image: 'https://source.unsplash.com/random/100x150',
-    },
-    {
-        id: 8,
-        image: 'https://source.unsplash.com/random/100x80',
-    },
-    {
-        id: 9,
-        image: 'https://source.unsplash.com/random/100x125',
-    },
-    {
-        id: 10,
-        image: 'https://source.unsplash.com/random/100x95',
-    },
-    {
-        id: 11,
-        image: 'https://source.unsplash.com/random/100x135',
-    },
-    {
-        id: 12,
-        image: 'https://source.unsplash.com/random/100x155',
-    },
-    {
-        id: 13,
-        image: 'https://source.unsplash.com/random/100x85',
-    },
-    {
-        id: 14,
-        image: 'https://source.unsplash.com/random/100x45',
-    },
-];
+import { Link } from 'react-router-dom';
+import { projects } from './CreatePage';
 
 export const LookbookPage = () => {
     return (
@@ -92,21 +27,23 @@ export const LookbookPage = () => {
                 marginTop="5"
                 marginBottom="10"
                 sx={{
-                    columnCount: { base: 1, md: 3, lg: 3 },
+                    columnCount: { base: 1, md: 2, lg: 3 },
                 }}
                 flexGrow={1}
             >
-                {projects.map((image) => (
-                    <Image
-                        key={image.id}
-                        cursor="pointer"
-                        w="100%"
-                        display="inline-block"
-                        mb={2}
-                        src={image.image}
-                        alt={image.id.toString()}
-                        borderRadius="sm"
-                    />
+                {projects.map((selectedProject) => (
+                    <Box as={Link} to={`/configurator?config=${selectedProject.url}`}>
+                        <Image
+                            key={selectedProject.id}
+                            w="100%"
+                            display="inline-block"
+                            mb={2}
+                            src={selectedProject.image}
+                            alt={selectedProject.id.toString()}
+                            borderRadius="lg"
+                            shadow={'xl'}
+                        />
+                    </Box>
                 ))}
             </Box>
         </VStack>
